0ccf0af79503e7203663733e349cd3d15e1d9b42
Julia K Abbott <jabbott19@my.whitworth.edu>
2019-03-16 11:48:46 -0700

    changed conversion from binary to hexcode

--- MP1/parser.py
+++ MP1/parser.py
@@ -1,19 +1,23 @@
 file_in = open("memory.txt", "r")
 file_out = open("memory.dat" , "w")
 
-commands = {}
+commands = {"lw" : 0x04, "sw" : 0x03, "sub": 0x02, "jump": 0x01, "HALT_AND_CATCH_FIRE": 0x20}
 labels = {}
 
 data = file_in.read()
 definitions = [ sublist.strip().split() for sublist in data.splitlines() ]
 
-for i in definitions:
-    pass
-for i in range (0, definitions):
-    NextHexCode = ""
-    if i == "/*":
-        while i != "*/":
-            i+= 1
-    if i == "jump":
+def translate(definitions):
+    for i in definitions:
         pass
-        #
+    for i in range (0, definitions):
+        NextHexCode = ""
+        if i == "/*":
+            while i != "*/":
+                i+= 1
+        if i == "jump":
+            pass
+  
+  
+file_in.close()
+file_out.close()      
