c1925898ef135fb4cc19812028dfb3ac8be37506
Julia K Abbott <jabbott19@my.whitworth.edu>
2019-03-18 09:38:48 -0700

    Updated parser, added test .txt file

--- MP1/parser.py
+++ MP1/parser.py
@@ -1,23 +1,34 @@
 file_in = open("memory.txt", "r")
 file_out = open("memory.dat" , "w")
 
-commands = {"lw" : 0x04, "sw" : 0x03, "sub": 0x02, "jump": 0x01, "HALT_AND_CATCH_FIRE": 0x20}
-labels = {}
+commands = {}
+commands["lw"] = ("1001", 5, 5, 16)
+commands["sw"] = ("1010", 5, 5, 16)
+commands["sub"] = ("0011", 5, 5, 5)
+labels = []
 
 data = file_in.read()
 definitions = [ sublist.strip().split() for sublist in data.splitlines() ]
-
+#use partition
 def translate(definitions):
+    translated = []
     for i in definitions:
         pass
-    for i in range (0, definitions):
-        NextHexCode = ""
-        if i == "/*":
-            while i != "*/":
-                i+= 1
-        if i == "jump":
-            pass
+    for j in range(0, len(definitions)):
+        for i in range (0, len(definitions)):
+            NextHexCode = ""
+            if definitions[j][i] =="//":
+                j += 1
+                continue
+            if definitions[j][i] == "jump":
+                labels.append(definitions[j][i + 1])
+            if definitions[j][i] ==";":
+                translated.append("\n")
+            if i == 0:
+                translated.append(commands[definitions[j][i]][0])
+    print(translated)
   
-  
+
+translate(definitions) 
 file_in.close()
 file_out.close()      
